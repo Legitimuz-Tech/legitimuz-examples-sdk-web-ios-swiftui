d307cbfedca0001bd4c0d49951432a4086baa955
Christian Santos <christian@legitimuz.com>
2025-04-15 09:54:11 -0300

    Initial Commit

---
+++ TestWebviewLegitimuzSDK/TestWebviewLegitimuzSDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestWebviewLegitimuzSDKApp.swift
+//  TestWebviewLegitimuzSDK
+//
+//  Created by Christian Santos on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct TestWebviewLegitimuzSDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestWebviewLegitimuzSDKTests/TestWebviewLegitimuzSDKTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestWebviewLegitimuzSDKTests.swift
+//  TestWebviewLegitimuzSDKTests
+//
+//  Created by Christian Santos on 4/15/25.
+//
+
+import Testing
+@testable import TestWebviewLegitimuzSDK
+
+struct TestWebviewLegitimuzSDKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
